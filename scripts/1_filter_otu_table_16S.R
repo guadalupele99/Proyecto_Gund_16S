1dd64cfb8bede88199842f0cbfbae05745dcb071
Guadalupe León <90155582+guadalupele99@users.noreply.github.com>
2022-05-15 20:58:39 -0500

    Update 1_filter_otu_table_16S.R

--- scripts/1_filter_otu_table_16S.R
+++ scripts/1_filter_otu_table_16S.R
@@ -1,28 +1,37 @@
-###
-### This script cleans the otu table by removing negative controls,
-### changing to relative abundance, removing 0 presence OTUs and changing the data to presence/absence
-###
+#
+### This script cleans the OTU table by removing negative controls,changing to relative abundance and removing OTUs with zero presence.
+#
 
-###
+#
 ### Load libraries
-###
+#
 
 library(phyloseq)
+library(ggplot2)
 
-###
+#
 ### Load data
-###
+#
 
+#
 ### Set working directory to source file
+#
 
+#
 ### Import biom data (includes OTU table, taxonomy table and sample data)
+#
 phyloseq_tables <- import_biom("taxonomy.biom")
-phyloseq_tables
 
+#
 ### Examine data
+#
+
+### Summary
+phyloseq_tables
 
 ### OTU table
 head(otu_table(phyloseq_tables))
+colnames(otu_table(phyloseq_tables))
 
 ### Taxonomy
 head(tax_table(phyloseq_tables))
@@ -33,7 +42,6 @@ sample_data(phyloseq_tables)
 ### View and change taxonomic ranks
 rank_names(phyloseq_tables)
 
-
 ### Change the names of the ranges
 colnames(tax_table(phyloseq_tables))<-c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species")
 head(tax_table(phyloseq_tables), n=6)
@@ -41,272 +49,147 @@ head(tax_table(phyloseq_tables), n=6)
 ### Sample data
 head(sample_data(phyloseq_tables), n=6)
 
-
-### Negative control filter
-
-### PCR1 Negative 1###
-
-### Subset PCR batch 1
-
-### If necessary, subset data by PCR batch. One negative control for each PCR, so those sequences need to be removed each PCR batch.
-
-pcr_batch = subset_samples(phyloseq_tables, pcr_batch == "batch_1")
-pcr_batch
-
-### Move PCR_batch to a dataframe to subtract things.
-
-pcr_batch_matrix = (as(otu_table(pcr_batch), "matrix"))
-pcr_batch_matrix
-
-pcr_batch_df = as.data.frame(pcr_batch_matrix)
-row.names(pcr_batch_df)
-colnames(pcr_batch_df)
-
-### Create a vector from the Negative control
-NC16sI=as.vector(pcr_batch_df$`NC16sI`)
-NC16sI
-
-
-### Insert the vector into a matrix
-test1=cbind(NC16sI)
-test1
-
-### Replicate the column for the number of samples
-test2 = test1[,rep(1,41)]
-test2
-
-
-### Subtract values from original matrix using the matrix based on repetition of negative control
-pcr_batch1_clean<-pcr_batch_matrix-test2
-pcr_batch1_clean
-
-### Replace all negative numbers with 0, since we can't have negative seqs reads
-pcr_batch1_cleaned  <-replace(pcr_batch1_clean, pcr_batch1_clean < 0, 0)
-pcr_batch1_cleaned[,1]
-
-##########################################################################################################################################
-##########################################################################################################################################
-
-### Do those steps again for each PCR batch to remove the contamination from the negative controls
-
-### PCR2 Negative 2
-
-pcr_batch2 = subset_samples(phyloseq_tables, pcr_batch == "batch_2")
-pcr_batch2
-
-### Move PCR_batch to a dataframe to subtract things.
-
-pcr_batch_matrix = (as(otu_table(pcr_batch2), "matrix"))
-pcr_batch_matrix
-
-pcr_batch2_df = as.data.frame(pcr_batch_matrix)
-row.names(pcr_batch2_df)
-colnames(pcr_batch2_df)
-
-### Create a vector from the Negative control
-
-NC16sS=as.vector(pcr_batch2_df$`NC16sS`)
-NC16sS
-
-### Insert the vector into a matrix
-test1=cbind(NC16sS)
-test1
-
-### Replicate the column for the number of samples
-test2 = test1[,rep(1,39)]
-test2
-
-### Subtract values from original matrix using the matrix based on repetition of negative control
-pcr_batch2_clean<-pcr_batch_matrix-test2
-pcr_batch2_clean
-
-### Replace all negative numbers with 0, since we can't have negative seqs reads
-pcr_batch2_cleaned <- replace(pcr_batch2_clean, pcr_batch2_clean < 0, 0)
-pcr_batch2_cleaned[,1]
-
-############################################################################################################################################
-############################################################################################################################################
-
-### PCR2 Negative 3
-
-pcr_batch3 = subset_samples(phyloseq_tables, pcr_batch == "batch_3")
-pcr_batch3
-
-### Move PCR_batch to a dataframe to subtract things
-
-pcr_batch_matrix = (as(otu_table(pcr_batch3), "matrix"))
-pcr_batch_matrix
-
-pcr_batch3_df = as.data.frame(pcr_batch_matrix)
-row.names(pcr_batch3_df)
-colnames(pcr_batch3_df)
-
-### Create a vector from the Negative control
-
-`NC16s-N`=as.vector(pcr_batch3_df$`NC16s-N`)
-`NC16s-N`
-
-### Insert the vector into a matrix
-test1=cbind(`NC16s-N`)
-test1
-
-### Replicate the column for the number of samples
-test2 = test1[,rep(1,5)]
-test2
-
-### Subtract values from original matrix using the matrix based on repetition of negative control
-pcr_batch3_clean<-pcr_batch_matrix-test2
-pcr_batch3_clean
-
-### Replace all negative numbers with 0, since we can't have negative seqs reads
-pcr_batch3_cleaned <- replace(pcr_batch3_clean, pcr_batch3_clean < 0, 0)
-pcr_batch3_cleaned[,1]
-
-### Now, we can merge all our new OTU tables back together, and reload as a phyloseq object along with the previous taxa table and metedata, since we haven't removed any samples rows or taxa columns.  Once in phyloseq, we can remove columns/OTUs that are now 0.
-### check that all objects are in the required format that phyloseq uses for each of them. Otu tables need to be all matrixes, metadata needs to be a data frame and taxonomy table needs to be a matrix
-### now merge otu tables:
-pcr_allbatches_cleaned <- cbind(pcr_batch1_cleaned, pcr_batch2_cleaned, pcr_batch3_cleaned)
-
-dim(pcr_allbatches_cleaned)
-
-### Create a new phyloseq object with all samples again.
-### Export tax table (as matrix) and meta table (as data frame) to be able to re-import them with the new otu table
-
-metadata_matrix = (as(sample_data(phyloseq_tables), "matrix"))
-metadata_matrix
-
-metadata_df = as.data.frame(metadata_matrix)
-rownames(metadata_df)
-colnames(metadata_df)
-
-taxdata_matrix = (as(tax_table(phyloseq_tables), "matrix"))
-taxdata_matrix
-
-### Reimport the clean otu table (matrix), meta data (data frame) and tax table (matrix) into a new phylseq object
-
-phyloseq_tables_cleaned <- phyloseq(otu_table(pcr_allbatches_cleaned, taxa_are_rows=TRUE),
-                                    sample_data(metadata_df),
-                                    tax_table(taxdata_matrix))
-
-### Check the object to make sure all the samples and OTUs came back.The columns numbers will be the same, even if they are empty now.
-phyloseq_tables_cleaned
-phyloseq_tables
-
-### Clean out OTUs rows that are no longer present.
-phyloseq_tables_cleaned <- prune_taxa(taxa_sums(phyloseq_tables_cleaned) > 0, phyloseq_tables_cleaned)
-
-### Clean out samples that are now empty (negative samples)
-phyloseq_tables_cleaned <- prune_samples(sample_sums(phyloseq_tables_cleaned) > 0, phyloseq_tables_cleaned)
-
-### Check the objects again
-phyloseq_tables_cleaned
-
-phyloseq_tables
-
-### Check if any OTUs have zero reads in any sample
-any(taxa_sums(phyloseq_tables_cleaned) == 0)
-
-### Read counts before and after cleaning
-
-taxa_sums(phyloseq_tables_cleaned)
-sum(taxa_sums(phyloseq_tables_cleaned)) # total number of reads: 4381109
-sum(taxa_sums(phyloseq_tables)) # total number of reads: 4383686
-sum(rowSums(pcr_allbatches_cleaned))  # total number of reads: 4381109
-
-########################################################################################################################################################
-########################################################################################################################################################
-
-### Step 2. DATA TRANSFORMATION
-### Transform read counts into  % relative abundances: because of concerns about rarefying NGS data
-### We will use the observed number of sequences normalized into relative abundance per sample.
-### The relative abundance is then multiplied by 1000 and rounded to the nearest integer to be used as count data.
-
-### Multiply by 1000 and transform to next integer so it looks like read count:
-phyloseq.rel = transform_sample_counts(phyloseq_tables_cleaned, function(x) 1000 * x/sum(x))
-otu_table(phyloseq.rel) = ceiling(otu_table(phyloseq.rel, "matrix")) # transform to next integer so it looks like read count
-otu_table(phyloseq.rel) # check otu table
-phyloseq.rel # check project
-
-### Check if any OTUs are still counted as relative abundance and not integer
-
-any(taxa_sums(phyloseq.rel) < 1)
-ntaxa(phyloseq.rel)
-
-### Check distribution of how many reads/OTU, reads/sample
-
-sum(taxa_sums(phyloseq.rel)) # total number of reads
-
-#######################################################################################################################################################
-#######################################################################################################################################################
-
-### Step 3. check distribution of how many reads/OTU, reads/sample: Plot number of reads per OTU / samples
-
-readsumsdf = data.frame(no.reads = sort(taxa_sums(phyloseq_tables), TRUE), sorted = 1:ntaxa(phyloseq_tables), type = "OTUs")
-readsumsdf = rbind(readsumsdf, data.frame(no.reads = sort(sample_sums(phyloseq_tables), TRUE), sorted = 1:nsamples(phyloseq_tables), type = "Samples"))
-title = ""
-p = ggplot(readsumsdf, aes(x = sorted, y = no.reads)) + geom_bar(stat = "identity")
-p + ggtitle(title) + scale_y_log10() + facet_wrap(~type, 1, scales = "free")
-
-readsumsdf = data.frame(no.reads = sort(taxa_sums(phyloseq.rel), TRUE), sorted = 1:ntaxa(phyloseq.rel), type = "OTUs")
-readsumsdf = rbind(readsumsdf, data.frame(no.reads = sort(sample_sums(phyloseq.rel), TRUE), sorted = 1:nsamples(phyloseq.rel), type = "Samples"))
-title = ""
-p = ggplot(readsumsdf, aes(x = sorted, y = no.reads)) + geom_bar(stat = "identity")
-p + ggtitle(title) + scale_y_log10() + facet_wrap(~type, 1, scales = "free")
-
-###########################################################################################################################################################
-###########################################################################################################################################################
-
-### Check for distribution of OTUs
-
-hist(log10(taxa_sums(phyloseq.rel)))
-
-############################################################################################################################################################
-############################################################################################################################################################
+###
+### Filter reads from Negative Control 
+### 
+
+### Source custom function to remove the reads from NC of all samples from a given PCR batch returning a matrix of the OTU table w/o the NC. 
+
+remove_NC<-function(phyloseq_tables, batch_id, NC_name){
+  
+  ### This functions removes the reads from NC of all samples from a given PCR batch
+  # returning a matrix of the OTU table w/o the NC
+  
+  ### Arguments:
+  ## phyloseq_tables: a phyloseq object result of importing a taxonomy.biom file.
+  ## batch_id: character vector with the name of the batch ID. Asumes it exists in a variable
+  # "pcr_batch" within the phyloseq tables.
+  ## NC_name: character vector with the name of the NC sample.
+  
+  ### Function:
+  ## Subset pcr batch
+  batch_id <<- batch_id # Assign batch_id to the global env because otherwise subset_samples do not work within a function
+  batch<- subset_samples(phyloseq_tables, pcr_batch == batch_id)
+  print("input data looks like:")
+  print(batch)
+  
+  ## Get OTU matrix
+  batch_OTU<-as.matrix(otu_table(batch))
+  
+  ## Get NC vector
+  NC<-as.vector(batch_OTU[,NC_name])
+  
+  ## Remove reads present in NC for each OTU
+  batch_clean<-batch_OTU-NC
+  
+  ## Replace negative numbers with 0 since we can't have negative reads
+  batch_clean<-replace(batch_clean, batch_clean < 0, 0)
+  
+  ## Tell the user what happened
+  print(paste("NC sample", NC_name, "was used to filter",  sum(NC), "reads in total"))
+  
+  ## Return results
+  batch_clean
+  
+}
+
+###################################################################################################################################################################
+###################################################################################################################################################################
+
+#
+### Filter NC reads in each PCR batch
+#
+
+### For PCR batch 1
+batch1_filtered<-remove_NC(phyloseq_tables=phyloseq_tables, batch_id = "batch_1", NC_name = "NC16sI")
+head(batch1_filtered, n=3)
+
+### For PCR batch 2
+batch2_filtered<-remove_NC(phyloseq_tables, batch_id = "batch_2", NC_name = "NC16sS")
+head(batch2_filtered, n=3)
+
+### For PCR batch 3
+batch3_filtered<-remove_NC(phyloseq_tables, batch_id = "batch_3", NC_name = "NC16s-N")
+head(batch3_filtered, n=3)
+
+#################################################################################################################################################################
+#################################################################################################################################################################
+
+#
+### Transform back to phyloseq object
+#
+
+### Bind together batches
+batches_filtered<-cbind(batch1_filtered, batch2_filtered, batch3_filtered)
+
+### Check OTU table
+head(batches_filtered, n=3)
+
+### Check dimensions match what went in
+dim(otu_table(phyloseq_tables))
+dim(batches_filtered)
+
+### Reimport data in new phyloseq object
+phyloseq_filtered<-phyloseq(otu_table(batches_filtered, taxa_are_rows = TRUE),
+                            sample_data(phyloseq_tables),
+                            tax_table(phyloseq_tables))
+### Check resulted data
+phyloseq_filtered
+head(otu_table(phyloseq_filtered), n=3)
+
+################################################################################################################################################################
+################################################################################################################################################################
 
 ###
-### Generate presence/absence table for beta-diversity analyses
+### Remove OTUs that are no longer present and NC samples
 ###
 
-### Transform sample counts to 0 and 1
+# Remove OTUs no longer present 
+phyloseq_cleaned <- prune_taxa(taxa_sums(phyloseq_filtered) > 0, phyloseq_filtered)
 
-binary_table = transform_sample_counts(phyloseq.rel, function(x, minthreshold=0){
-  x[x > minthreshold] <- 1
-  return(x)})
+# Remove samples that are now have no reads (NC and any failed sample)
+phyloseq_cleaned <- prune_samples(sample_sums(phyloseq_cleaned) > 0, phyloseq_cleaned)
 
-### Check
+# Check that no OTUs have 0 reads in any sample
+any(taxa_sums(phyloseq_cleaned) == 0)
 
-head(otu_table(binary_table))
+### Compare what came in and was filtered at each step:
 
-###############################################################################################################################################################
-###############################################################################################################################################################
+# Original data
+phyloseq_tables
+sum(otu_table(phyloseq_tables)) # total number of reads
 
-### Remove OTUs that appear only in 1 sample (using presence/absence)
+# Filtering reads from NC
+phyloseq_filtered
+sum(otu_table(phyloseq_filtered))
 
-any(taxa_sums(binary_table) == 1)
-otu_table(prune_taxa(taxa_sums(binary_table) <= 1, binary_table))
-binary_table_OTU2 <- prune_taxa(taxa_sums(binary_table) > 1, binary_table)
+# Removing 0 reads OTUs and samples
+phyloseq_cleaned
+sum(otu_table(phyloseq_cleaned))
 
-binary_table
-binary_table_OTU2
+##############################################################################################################################################################
+##############################################################################################################################################################
 
-###############################################################################################################################################################
-###############################################################################################################################################################
+#
+### Generate table of relative abundances
+# 
 
-### TO FIX! Right now calculate read sum and not taxa sum
-### Remove OTUs that appear only in 1 sample (using presence/absence)
-### any(taxa_sums(phyloseq.rel) == 1)
-### otu_table(prune_taxa(taxa_sums(phyloseq.rel) <= 1, phyloseq.rel))
-### phyloseq.rel_OTU2 <- prune_taxa(taxa_sums(phyloseq.rel) > 1, phyloseq.rel)
+### Transform read counts into relative abundances
 
-phyloseq.rel
+### Estimate proportion of reads per OTU per sample relative to the total sum of reads per each OTU
+phyloseq_rel.abundance<- transform_sample_counts(phyloseq_cleaned, function(x) 10000 * x/sum(x))
 
-### phyloseq.rel_OTU2
-sample_sums(phyloseq_tables)
-sample_sums(phyloseq_tables_cleaned)
-sample_sums(phyloseq.rel)
+### Transform to next integer so it looks like "read count"
+otu_table(phyloseq_rel.abundance) <- ceiling(otu_table(phyloseq_rel.abundance, "matrix"))
 
-### FINAL OTU TABLES OBTAINED FOR FURTHER ANALYSIS
-### phyloseq.rel (OTU table with relative abundances - corrected in count)
-### binary_table
-### phyloseq.rel_OTU2
-### binary_table_OTU2
+#
+### Compare vs original
+#
+
+### Number of reads
+otu_table(phyloseq_cleaned)[1:3,1:3]
+otu_table(phyloseq_rel.abundance)[1:3,1:3]
+
+### Phyloseq objects
+phyloseq_cleaned
+phyloseq_rel.abundance
