626a39caf053faee9fb2574eef416529038ec5ab
Guadalupe León <90155582+guadalupele99@users.noreply.github.com>
2023-05-18 20:43:33 -0600

    Update 3_filter_otu_table_soil_and_local_maize_seed.R

--- scripts/3_filter_otu_table_soil_and_local_maize_seed.R
+++ scripts/3_filter_otu_table_soil_and_local_maize_seed.R
@@ -3,83 +3,82 @@
 #
 
 #
-### 
+### Bacterial communities in local seed associated to low humidity
 #
 
 ### Filter soil, landrace (local seed) and Tzotzil (low humidity)
-phyloseq_rel.abundance_soil_tzotzil_landrace<- subset_samples(phyloseq_rel.abundance_soil, source== "Soil" & farmer_type== "Tzotzil" & maize_type== "Landrace")
+phyloseq_rel.abundance_soil_local seed_low humidity<- subset_samples(phyloseq_rel.abundance_soil, source== "Soil" & farmer_type== "Tzotzil" & maize_type== "Landrace")
 
 ### Remove OTUs no longer present
-phyloseq_rel.abundance_soil_tzotzil_landrace <- prune_taxa(taxa_sums(phyloseq_rel.abundance_soil_tzotzil_landrace) > 0, phyloseq_rel.abundance_soil_tzotzil_landrace)
+phyloseq_rel.abundance_soil_local seed_low humidity <- prune_taxa(taxa_sums(phyloseq_rel.abundance_soil_local seed_low humidity) > 0, phyloseq_rel.abundance_soil_local seed_low humidity)
 
 #
 ### Final tables for further analyses are:
 #
 
-### Soil, landrace maize and Tzotzil
+### Soil, local seed and low humidity
 
 ### Summary
-phyloseq_rel.abundance_soil_tzotzil_landrace
+phyloseq_rel.abundance_soil_local seed_low humidity
 
 ###########################################
 
 #
-###  Relative abundance table with OTU of bacterial communities associated with the local seed in the low humidity
+###  Relative abundance table with OTU of bacterial communities associated with the local seed in low humidity
 #
 
-OTU38 = as(otu_table(phyloseq_rel.abundance_soil_tzotzil_landrace), "matrix")
-OTUdf = as.data.frame(OTU38)
+OTU3 = as(otu_table(phyloseq_rel.abundance_soil_local seed_low humidity), "matrix")
+OTUdf = as.data.frame(OTU3)
 
-write.csv(OTU38, file = "relative_abundance_soil_TZOTZIL_LANDRACE.csv")
+write.csv(OTU3, file = "relative_abundance_soil_local seed_low humidity.csv")
 
 #
-### Relative abundance table with taxa of bacterial communities associated with the local seed in the low humidity
+### Relative abundance table with taxa of bacterial communities associated with the local seed in low humidity
 #
 
-OTU39 = as(tax_table(phyloseq_rel.abundance_soil_tzotzil_landrace), "matrix")
-OTUdf = as.data.frame(OTU39)
+OTU4 = as(tax_table(phyloseq_rel.abundance_soil_local seed_low humidity), "matrix")
+OTUdf = as.data.frame(OTU4)
 
-write.csv(OTU39, file = "tax_soil_local seed_low humidity.csv")
-
+write.csv(OTU4, file = "tax_soil_local seed_low humidity.csv")
 
 ###################################################################################################################################################################
 ###################################################################################################################################################################
 
 #
-### Soil and type of maize
+### Bacterial communities in local seed associated to medium humidity
 #
 
-### Filter soil and type of maize hybrid
-phyloseq_rel.abundance_soil_hybrid<- subset_samples(phyloseq_rel.abundance_soil, source== "Soil" & maize_type== "Hybrid")
+### Filter soil, landrace (local seed) and Mestizo (medium humidity)
+phyloseq_rel.abundance_soil_local seed_medium humidity<- subset_samples(phyloseq_rel.abundance_soil, source== "Soil" & farmer_type== "Mestizo" & maize_type== "Landrace")
 
 ### Remove OTUs no longer present
-phyloseq_rel.abundance_soil_hybrid <- prune_taxa(taxa_sums(phyloseq_rel.abundance_soil_hybrid) > 0, phyloseq_rel.abundance_soil_hybrid)
+phyloseq_rel.abundance_soil_local seed_medium humidity <- prune_taxa(taxa_sums(phyloseq_rel.abundance_soil_local seed_medium humidity) > 0, phyloseq_rel.abundance_soil_local seed_medium humidity)
 
 #
 ### Final tables for further analyses are:
 #
 
-### Soil and type of maize hybrid
+### Soil, local seed and medium humidity
 
 ### Summary
-phyloseq_rel.abundance_soil_hybrid
+phyloseq_rel.abundance_soil_local seed_medium humidity
 
-###########################################
+#############################################################
 
 #
-###  Relative abundance table with OTU of bacterial communities associated with soil and hybrid maize
+###  Relative abundance table with OTU of bacterial communities associated with the local seed in medium humidity
 #
 
-OTU5 = as(otu_table(phyloseq_rel.abundance_soil_hybrid), "matrix")
+OTU5 = as(otu_table(phyloseq_rel.abundance_soil_local seed_medium humidity), "matrix")
 OTUdf = as.data.frame(OTU5)
 
-write.csv(OTU5, file = "relative_abundance_soil_hybrid.csv")
+write.csv(OTU5, file = "relative_abundance_soil_local seed_medium humidity.csv")
 
 #
-### Relative abundance table with taxa of bacterial communities associated with soil and hybrid maize
+### Relative abundance table with taxa of bacterial communities associated with the local seed in medium humidity
 #
 
-OTU6 = as(tax_table(phyloseq_rel.abundance_soil_hybrid), "matrix")
+OTU6 = as(tax_table(phyloseq_rel.abundance_soil_local seed_medium humidity), "matrix")
 OTUdf = as.data.frame(OTU6)
 
-write.csv(OTU6, file = "tax_soil_hybrid.csv")
+write.csv(OTU6, file = "tax_soil_local seed_medium humidity.csv")
